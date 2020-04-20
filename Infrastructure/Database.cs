40465d7f2c2cf5d2b3fbb97e901941c288401ec5
MikeM-BrainWare <63931884+MikeM-BrainWare@users.noreply.github.com>
2020-04-20 11:55:24 -0400

    Add files via upload
    Removed embedded SQL and add stored procedures to mdf
    Configured solution to run web project
    deleted API order controller, infrastructure service and created new OrderContext model to handle BL
    Redesigned Homecontroller by adding method to call BL and pass order list generated by new BL model
    Redesigned Home Index to present passed in model data using C# razor script instead of api reference in jscript
    Made sure existing Test controller would pass existing criteria

---
+++ Infrastructure/Database.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Infrastructure
+{
+    using System.Data;
+    using System.Data.Common;
+    using System.Data.SqlClient;
+
+    public class Database
+    {
+        private readonly SqlConnection _connection;
+
+        public Database()
+        {
+            _connection = new SqlConnection("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=BrainWAre;Integrated Security=SSPI;AttachDBFilename=C:\\BrainWare\\Web\\App_Data\\BrainWare.mdf");
+
+            _connection.Open();
+        }
+
+
+        public DbDataReader ExecuteReader(string query)
+        {
+            SqlCommand cmd = new SqlCommand(query, _connection); // use stored procedure to eliminate embedded SQL
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            //var sqlQuery = new SqlCommand(query, _connection);
+
+            return cmd.ExecuteReader();
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            var sqlQuery = new SqlCommand(query, _connection);
+
+            return sqlQuery.ExecuteNonQuery();
+        }
+
+    }
+}
